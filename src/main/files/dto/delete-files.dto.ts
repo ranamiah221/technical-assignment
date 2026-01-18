70e171200069fa3a70cfff7c58618940e5e1a899
ranamiah221 <ranarasul21@gmail.com>
2026-01-18 09:43:08 +0600

    project setup

--- src/main/files/dto/delete-files.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsUUID } from 'class-validator';
-
-export class DeleteFilesDto {
-  @ApiProperty({ type: [String] })
-  @IsArray()
-  @IsUUID('4', { each: true })
-  fileIds: string[];
-}
